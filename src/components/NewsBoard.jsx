1b708c3dcade0f3184e745f72bf2137aad5644c7
Razique Mk <raziquemk2002@gmail.com>
2024-08-12 14:33:20 +0530

    final commit

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -8,9 +8,11 @@ const NewsBoard = ({category}) => {
   const[articles , setArticles] = useState([]);
 
   useEffect(() => {
+    const VITE_API_KEY="45fa04d730cd49ffaa9a44af80ba162f";
+
 
     
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${VITE_API_KEY}`
 
     axios.get(url)
     .then((res) => setArticles(res.data.articles))
