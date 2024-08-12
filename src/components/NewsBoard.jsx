5240e7bfddc12f23ef5b7e1e4a48e6483fbaa5bf
Razique Mk <raziquemk2002@gmail.com>
2024-08-12 14:26:23 +0530

    intial commit

---
+++ src/components/NewsBoard.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+
+
+const NewsBoard = ({category}) => {
+
+  const[articles , setArticles] = useState([]);
+
+  useEffect(() => {
+
+    
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+
+    axios.get(url)
+    .then((res) => setArticles(res.data.articles))
+    .catch((error) => console.eror("No news found",error))
+
+  },[category]);
+
+  console.log(articles)
+
+
+
+
+
+  return (
+    <div>
+      <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+
+      {articles.map((news,index) => {
+        return <NewsItem key={index} title = {news.title} description = {news.description} src = {news.urlToImage} url = {news.url}/>
+      } )}
+    </div>
+  )
+}
+
+export default NewsBoard
